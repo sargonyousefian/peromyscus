1dfaa2374701370af466a63aebb3c3a75f7ad84a
Sargon Yousefian <sargon.yousefian@gmail.com>
2026-08-13 10:40:42 -0400

    Create geno_file_from_entropy.sh

---
+++ geno_file_from_entropy.sh
@@ -0,0 +1,7 @@
+/home/sydt/scratch/entropy/unique_individual_top_5000/entropy_results
+
+/home/sydt/projects/def-mcfarlas/software/estpost.entropy -p gprob -s 0 \
+  peromyscus_ind999_top5000_k2_225k_rep1.hdf5 \
+  peromyscus_ind999_top5000_k2_225k_rep2.hdf5 \
+  peromyscus_ind999_top5000_k2_225k_rep3.hdf5 \
+  -o genoest.txt
